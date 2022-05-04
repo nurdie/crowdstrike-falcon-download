9c34a4c26922f6f50fee2e912f1d0588d0cc529e
Daniel B Shaw <daniel@webreggy.com>
2022-05-03 19:43:40 -0500

    Update secrets.py for AWS S3

--- secrets.py
+++ secrets.py
@@ -1,3 +1,4 @@
 from credlib import credential
 
 csfalcon = credential("localhost", "API_CLIENT_ID_HERE", "APT_CLIENT_SECRET_HERE")
+s3aws = credential("S3_BUCKET_NAME", "AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY")
