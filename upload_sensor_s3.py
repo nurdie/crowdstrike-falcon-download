54f7497df5ddc35ad31c4a87812becc85bb390d1
Daniel B Shaw <daniel@webreggy.com>
2022-05-03 19:41:56 -0500

    Add AWS S3 Sensor Upload

---
+++ upload_sensor_s3.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+from secrets import s3aws
+bucket, keyid, accesskey = s3aws.get(s3aws)
+import boto3
+import os, fnmatch
+
+# Globals
+# Upload sensors for the following operating systems
+OPERATING_SYSTEM_FILTER = [
+        "windows",
+        "linux",
+        "mac"
+        ]
+
+def find(pattern, path):
+    result = []
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            if fnmatch.fnmatch(name, pattern):
+                result.append(os.path.join(root, name))
+    return result
+
+session = boto3.Session(
+    aws_access_key_id=keyid,
+    aws_secret_access_key=accesskey,
+)
+s3 = session.resource('s3')
+# Filename - File to upload
+# Bucket - Bucket to upload to (the top level directory under AWS S3)
+# Key - S3 object name (can contain subdirectories). If not specified then file_name is used
+for ostype in OPERATING_SYSTEM_FILTER:
+  try:
+    sensor = os.path.basename(str(find("crowdstrike-falcon-sensor-{}*".format(ostype), "./")[0]))
+  except Exception as msg:
+    print("Error retrieving file: ")
+    print(msg)
+    continue
+  print("Found {} - Uploading...".format(sensor))
+  try:
+    s3.meta.client.upload_file(Filename=sensor, Bucket=bucket, Key=sensor)
+  except Exception as msg:
+    print("Error uploading file: ")
+    print(msg)
+    continue
